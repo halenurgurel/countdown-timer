f78f98b27c1dba02b5525eb296a4e06fb236fce4
Halenur Gurel <halenur.gurel@hotmail.com>
2026-01-11 17:00:33 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/countdown-timer/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve("./index.html"),
-      },
-    },
-  },
+  base: "/",
 });
